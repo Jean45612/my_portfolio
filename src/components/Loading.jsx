b4d798f5a9926b30753a1a4c7c7452fcc405b54d
Jean <puchit01200@gmail.com>
2021-07-04 22:08:30 -0500

    Refactorizacion Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,15 +4,18 @@ import { useEffect } from "react";
 import "../assets/css/loading.css";
 
 const Loading = (props) => {
+  const duration = 5;
+  const styles = { animation: `fadein ${duration}s`};
+
   useEffect(() => {
     setTimeout(() => {
       props.setisLoading(false);
-    }, 5000);
+    }, duration * 1000);
   });
 
   return (
     <div className="loader">
-      <div className="datos">
+      <div className="datos" style={styles}>
         <p>Hola mundo</p>
       </div>
     </div>
