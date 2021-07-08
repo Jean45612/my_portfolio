fc4d832b314163dc1a80fcd9282de5af8561376d
Jean <puchit01200@gmail.com>
2021-07-08 18:23:55 -0500

    cargo dinamicamente las imagenes

--- src/components/views/Portafolio.jsx
+++ src/components/views/Portafolio.jsx
@@ -4,27 +4,29 @@ import "../../assets/css/portafolio.css";
 import ModalPortafolio from "../../components/common/ModalPortafolio";
 import proyectos from "../../utils/portafolio.json";
 
-import imgEncuestas from "../../assets/images/portafolio/encuestas/encuestas.PNG";
-import imgHistoria from "../../assets/images/portafolio/historia_clinica/paciente.PNG";
-import imgProducto from "../../assets/images/portafolio/logistico/producto.PNG";
-import imgPedido from "../../assets/images/portafolio/pedidos/pedido.PNG";
-import imgProyecto from "../../assets/images/portafolio/proyectos/tarea.PNG";
-import imgTarjetas from "../../assets/images/portafolio/tarjetas/tar1.PNG";
-
 function Portafolio() {
   const [showModal, setShowModal] = useState(false);
 
   const handleShow = () => setShowModal(true);
 
+  const requestImageFile = require.context(
+    "../../assets/images/portafolio",
+    true
+  );
+
   return (
     <div id="portafolio">
       <h3 className="text-center">Portafolio</h3>
       <div className="row mt-4 mx-0">
-        {/* {proyectos.map((p) => {
+        {proyectos.map((p) => {
           return (
-            <div className="col-lg-4 col-sm-6 proyecto" onClick={handleShow}>
+            <div
+              className="col-lg-4 col-sm-6 proyecto"
+              onClick={handleShow}
+              key={p.id}
+            >
               <img
-                src={p.imagen}
+                src={requestImageFile(`./${p.imagen}`).default}
                 alt=""
                 loading="lazy"
                 data-aos="zoom-in"
@@ -36,90 +38,7 @@ function Portafolio() {
               </div>
             </div>
           );
-        })} */}
-        <div className="col-lg-4 col-sm-6 proyecto" onClick={handleShow}>
-          <img
-            src={imgEncuestas}
-            alt=""
-            loading="lazy"
-            data-aos="zoom-in"
-            data-aos-duration="500"
-          />
-          <div className="text-image">
-            <h4>Encuestas</h4>
-            <h5>Angular / Laravel</h5>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-sm-6 proyecto" onClick={handleShow}>
-          <img
-            src={imgHistoria}
-            alt=""
-            loading="lazy"
-            data-aos="zoom-in"
-            data-aos-duration="500"
-          />
-          <div className="text-image">
-            <h4>Historia clínica</h4>
-            <h5>Laravel</h5>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-sm-6 proyecto" onClick={handleShow}>
-          <img
-            src={imgProducto}
-            alt=""
-            loading="lazy"
-            data-aos="zoom-in"
-            data-aos-duration="500"
-          />
-          <div className="text-image">
-            <h4>Sistema logístico</h4>
-            <h5>Vue / Laravel</h5>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-sm-6 proyecto" onClick={handleShow}>
-          <img
-            src={imgTarjetas}
-            alt=""
-            loading="lazy"
-            data-aos="zoom-in"
-            data-aos-duration="500"
-          />
-          <div className="text-image">
-            <h4>Tarjetas digitales</h4>
-            <h5>Angular / Laravel</h5>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-sm-6 proyecto" onClick={handleShow}>
-          <img
-            src={imgProyecto}
-            alt=""
-            loading="lazy"
-            data-aos="zoom-in"
-            data-aos-duration="500"
-          />
-          <div className="text-image">
-            <h4>Gestión de proyectos</h4>
-            <h5>Vue / Laravel</h5>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-sm-6 proyecto" onClick={handleShow}>
-          <img
-            src={imgPedido}
-            alt=""
-            loading="lazy"
-            data-aos="zoom-in"
-            data-aos-duration="500"
-          />
-          <div className="text-image">
-            <h4>Pedidos</h4>
-            <h5>Angular / Laravel</h5>
-          </div>
-        </div>
+        })}
       </div>
       <ModalPortafolio showModal={showModal} setShowModal={setShowModal} />
     </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './assets/css/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/fonts/Amattera/Amattera-Million.ttf';
-import './assets//fonts/AvenueX/AvenueX-Regular.otf';
+import './assets/fonts/AvenueX/AvenueX-Regular.otf';
 import 'aos/dist/aos.css';
 
 ReactDOM.render(
