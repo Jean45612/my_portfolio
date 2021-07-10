9e0601380c843ba107554e11394ecae48e7d6210
Jean <puchit01200@gmail.com>
2021-07-10 18:28:22 -0500

    Contacto terminado

--- src/components/views/Contacto.jsx
+++ src/components/views/Contacto.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { useState } from "react";
 import useSimpleReactValidator from "../../hooks/useSimpleReactValidator";
 import swal from "../../utils/swal";
+import emailjs from "emailjs-com";
+import mailConfig from "../../config/mail.json";
 
-function Contacto() {
+function CorreoEnviado(props) {
   const [form, setForm] = useState({
     nombre: "",
     email: "",
@@ -18,12 +20,35 @@ function Contacto() {
     e.preventDefault();
 
     if (validator.allValid()) {
-      console.log("submitear", form);
-      swal.alerta("Bienvenido", "success");
+      emailjs
+        .sendForm(
+          mailConfig.serviceId,
+          mailConfig.templatePortafolioId,
+          e.target,
+          mailConfig.userId
+        )
+        .then(
+          (result) => {
+            swal.alerta("Su correo fue enviado correctamente", "success");
+            limpiar();
+          },
+          (error) => {
+            console.log(error.text);
+            limpiar();
+          }
+        );
     } else {
-      console.log("error");
       validator.showMessages();
     }
+  };
+
+  const limpiar = () => {
+    setForm({
+      nombre: "",
+      email: "",
+      mensaje: "",
+    });
+    props.setIsSent(true);
   };
 
   const handleInputChange = (e) => {
@@ -34,13 +59,8 @@ function Contacto() {
   };
 
   return (
-    <div className="bg-info" id="contactame">
-      <h3 className="text-center py-4">Contacto</h3>
-      <form
-        className="container bg-info"
-        data-aos="fade-in"
-        onSubmit={handleSubmit}
-      >
+    <React.Fragment>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="form-group col-sm-6">
             <label>Nombre</label>
@@ -84,6 +104,41 @@ function Contacto() {
           Enviar
         </button>
       </form>
+    </React.Fragment>
+  );
+}
+
+function CorreoNoEnviado() {
+  return (
+    <React.Fragment>
+      <div className="alert alert-light" role="alert">
+        <h5 className="alert-heading">Saludos</h5>
+        <p>Pronto me pondre en contacto con usted.</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Contacto() {
+  const [isSent, setIsSent] = useState(false);
+
+  return (
+    <div id="contactame">
+      <div className="bg-info">
+        <h3 className="text-center py-4">Contacto</h3>
+        <div className="container pb-4" data-aos="fade-in">
+          {!isSent ? (
+            <CorreoEnviado setIsSent={setIsSent} />
+          ) : (
+            <CorreoNoEnviado />
+          )}
+        </div>
+      </div>
+
+      <div className="derechos">
+        <p>Desarrollado y diseñado por Jean Pierre Acosta Tomaylla</p>
+        <span> ©2021. Todos los derechos reservados.</span>
+      </div>
     </div>
   );
 }
